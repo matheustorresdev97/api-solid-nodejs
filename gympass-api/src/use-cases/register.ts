ff5ae7e5701e31f19762bf044f35def95af06167
matheustorresdev97 <matheustorresdev@gmail.com>
2025-10-04 21:36:35 -0300

    configurando vitest e criando teste unitario

--- gympass-api/src/use-cases/register.ts
+++ gympass-api/src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcrypt";
+import { User } from "@prisma/client";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseRequest {
@@ -7,11 +8,18 @@ interface RegisterUseCaseRequest {
   email: string;
   password: string;
 }
+interface RegisterUseCaseResponse {
+  user: User;
+}
 
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -20,10 +28,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }
