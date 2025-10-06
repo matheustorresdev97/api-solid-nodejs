cc31678fec7e3a84361fe408dba3214998810049
matheustorresdev97 <matheustorresdev@gmail.com>
2025-10-06 19:59:35 -0300

    controllers - criação de academia

---
+++ gympass-api/src/http/controllers/users/routes.ts
@@ -0,0 +1,15 @@
+import { FastifyInstance } from "fastify";
+
+import { verifyJwt } from "../../middlewares/verity-jwt";
+
+import { authenticate } from "./authenticate";
+import { profile } from "./profile";
+import { register } from "./register";
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post("/users", register);
+
+  app.post("/sessions", authenticate);
+
+  app.get("/me", { onRequest: [verifyJwt] }, profile);
+}
