67ef663d6ca08e6696dc1d85faffdf565887d66f
matheustorresdev97 <matheustorresdev@gmail.com>
2025-10-07 08:05:10 -0300

    implementação do refresh token

--- gympass-api/src/http/controllers/users/routes.ts
+++ gympass-api/src/http/controllers/users/routes.ts
@@ -5,11 +5,16 @@ import { verifyJwt } from "../../middlewares/verity-jwt";
 import { authenticate } from "./authenticate";
 import { profile } from "./profile";
 import { register } from "./register";
+import { refresh } from "./refresh";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
 
   app.post("/sessions", authenticate);
+
+  app.patch('/token/refresh', refresh);
+
+
 
   app.get("/me", { onRequest: [verifyJwt] }, profile);
 }
